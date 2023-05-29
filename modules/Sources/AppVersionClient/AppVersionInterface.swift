1e1232f6aa6de8279e26a98f1c873fba3e0a1421
Lukas Korba <LukasKorba@users.noreply.github.com>
2023-05-29 08:28:36 +0200

    [#739] AppVersion Dependency Package (#742)
    - AppVersion dependency moved to a separate package

---
+++ modules/Sources/AppVersionClient/AppVersionInterface.swift
@@ -0,0 +1,20 @@
+//
+//  AppVersionInterface.swift
+//  secant-testnet
+//
+//  Created by Lukáš Korba on 12.11.2022.
+//
+
+import ComposableArchitecture
+
+extension DependencyValues {
+    public var appVersion: AppVersionClient {
+        get { self[AppVersionClient.self] }
+        set { self[AppVersionClient.self] = newValue }
+    }
+}
+
+public struct AppVersionClient {
+    public let appVersion: () -> String
+    public let appBuild: () -> String
+}
