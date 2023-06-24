Electric-Coin-Company/zashi-ios

[#766] Modularize batch of leaf features Vol III (#768)
Following features have been modularized:
- OnboardingFlow
- ImportWallet
- SendFlow
- Sandbox
- crashReporter TODO added

modules/Sources/Features/OnboardingFlow/OnboardingFlowView.swift
@@ -1,0 +1,98 @@
+//
+//  OnboardingScreen.swift
+//  secant-testnet
+//
+//  Created by Adam Stener on 11/7/21.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import UIComponents
+
+public struct OnboardingScreen: View {
+    let store: OnboardingFlowStore
+
+    public init(store: OnboardingFlowStore) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                ZStack {
+                    OnboardingHeaderView(
+                        store: store.scope(
+                            state: { state in
+                                OnboardingHeaderView.ViewState(
+                                    walletConfig: state.walletConfig,
+                                    isInitialStep: state.isInitialStep,
+                                    isFinalStep: state.isFinalStep
+                                )
+                            },
+                            action: { action in
+                                switch action {
+                                case .back: return .back
+                                case .skip: return .skip
+                                }
+                            }
+                        )
+                    )
+                    .zIndex(1)
+                    
+                    OnboardingContentView(store: store)
+                }
+                
+                Spacer()
+                
+                OnboardingFooterView(store: store)
+                
+                Spacer()
+            }
+            .navigationBarHidden(true)
+            .applyScreenBackground()
+            .onAppear { viewStore.send(.onAppear) }
+        }
+    }
+}
+
+// MARK: - Previews
+
+struct OnboardingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingScreen(
+            store: Store(
+                initialState: OnboardingFlowReducer.State(
+                    walletConfig: .default,
+                    importWalletState: .placeholder
+                ),
+                reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+            )
+        )
+        .preferredColorScheme(.light)
+        .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+
+        OnboardingScreen(
+            store: Store(
+                initialState: OnboardingFlowReducer.State(
+                    walletConfig: .default,
+                    importWalletState: .placeholder
+                ),
+                reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+            )
+        )
+        .preferredColorScheme(.light)
+        .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+
+        OnboardingScreen(
+            store: Store(
+                initialState: OnboardingFlowReducer.State(
+                    walletConfig: .default,
+                    importWalletState: .placeholder
+                ),
+                reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+            )
+        )
+        .preferredColorScheme(.light)
+        .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))
+    }
+}

modules/Sources/Features/OnboardingFlow/PlainOnboardingView.swift
@@ -1,0 +1,78 @@
+//
+//  PlainOnboardingView.swift
+//  secant
+//
+//  Created by Francisco Gindre on 3/13/23.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import Generated
+import ImportWallet
+
+public struct PlainOnboardingView: View {
+    let store: OnboardingFlowStore
+    let animationDuration: CGFloat = 0.8
+
+    public init(store: OnboardingFlowStore) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                VStack(alignment: .leading, spacing: 80) {
+                    Text(L10n.PlainOnboarding.title)
+                        .fontWeight(.heavy)
+                        .font(.largeTitle)
+                    
+                    Text(L10n.PlainOnboarding.caption)
+                        .fontWeight(.semibold)
+                        .font(.body)
+                }
+                .padding(0)
+                
+                Spacer()
+                
+                Button(L10n.PlainOnboarding.Button.createNewWallet) {
+                    viewStore.send(.createNewWallet, animation: .easeInOut(duration: animationDuration))
+                }
+                .activeButtonStyle
+                
+                Button(L10n.PlainOnboarding.Button.restoreWallet) {
+                    viewStore.send(.importExistingWallet, animation: .easeInOut(duration: animationDuration))
+                }
+                .frame(height: 80)
+                .foregroundColor(.black)
+            }
+            .padding(.all)
+            .navigationLinkEmpty(
+                isActive: viewStore.bindingForDestination(.importExistingWallet),
+                destination: {
+                    ImportWalletView(
+                        store: store.scope(
+                            state: \.importWalletState,
+                            action: OnboardingFlowReducer.Action.importWallet
+                        )
+                    )
+                }
+            )
+        }
+    }
+}
+
+struct PlainOnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            PlainOnboardingView(
+                store: Store(
+                    initialState: OnboardingFlowReducer.State(
+                        walletConfig: .default,
+                        importWalletState: .placeholder
+                    ),
+                    reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+                )
+            )
+        }
+    }
+}

nighthawk-apps/nighthawk-ios-wallet

modules/Sources/Features/OnboardingFlow/OnboardingFlowView.swift
@@ -1,0 +1,104 @@
+//
+//  OnboardingScreen.swift
+//  secant-testnet
+//
+//  Created by Adam Stener on 11/7/21.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import UIComponents
+
+public struct OnboardingScreen: View {
+    let store: OnboardingFlowStore
+
+    public init(store: OnboardingFlowStore) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                ZStack {
+                    OnboardingHeaderView(
+                        store: store.scope(
+                            state: { state in
+                                OnboardingHeaderView.ViewState(
+                                    walletConfig: state.walletConfig,
+                                    isInitialStep: state.isInitialStep,
+                                    isFinalStep: state.isFinalStep
+                                )
+                            },
+                            action: { action in
+                                switch action {
+                                case .back: return .back
+                                case .skip: return .skip
+                                }
+                            }
+                        )
+                    )
+                    .zIndex(1)
+                    
+                    OnboardingContentView(store: store)
+                }
+                
+                Spacer()
+                
+                OnboardingFooterView(store: store)
+                
+                Spacer()
+            }
+            .navigationBarHidden(true)
+            .applyScreenBackground()
+            .onAppear { viewStore.send(.onAppear) }
+        }
+    }
+}
+
+// MARK: - Previews
+
+struct OnboardingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingScreen(
+            store: Store(
+                initialState: OnboardingFlowReducer.State(
+                    walletConfig: .default,
+                    importWalletState: .placeholder,
+                    nhImportWalletState: .placeholder,
+                    walletCreatedState: .placeholder
+                ),
+                reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+            )
+        )
+        .preferredColorScheme(.light)
+        .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+
+        OnboardingScreen(
+            store: Store(
+                initialState: OnboardingFlowReducer.State(
+                    walletConfig: .default,
+                    importWalletState: .placeholder,
+                    nhImportWalletState: .placeholder,
+                    walletCreatedState: .placeholder
+                ),
+                reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+            )
+        )
+        .preferredColorScheme(.light)
+        .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+
+        OnboardingScreen(
+            store: Store(
+                initialState: OnboardingFlowReducer.State(
+                    walletConfig: .default,
+                    importWalletState: .placeholder,
+                    nhImportWalletState: .placeholder,
+                    walletCreatedState: .placeholder
+                ),
+                reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+            )
+        )
+        .preferredColorScheme(.light)
+        .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))
+    }
+}

modules/Sources/Features/OnboardingFlow/PlainOnboardingView.swift
@@ -1,0 +1,80 @@
+//
+//  PlainOnboardingView.swift
+//  secant
+//
+//  Created by Francisco Gindre on 3/13/23.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import Generated
+import ImportWallet
+
+public struct PlainOnboardingView: View {
+    let store: OnboardingFlowStore
+    let animationDuration: CGFloat = 0.8
+
+    public init(store: OnboardingFlowStore) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                VStack(alignment: .leading, spacing: 80) {
+                    Text(L10n.PlainOnboarding.title)
+                        .fontWeight(.heavy)
+                        .font(.largeTitle)
+                    
+                    Text(L10n.PlainOnboarding.caption)
+                        .fontWeight(.semibold)
+                        .font(.body)
+                }
+                .padding(0)
+                
+                Spacer()
+                
+                Button(L10n.PlainOnboarding.Button.createNewWallet) {
+                    viewStore.send(.createNewWallet, animation: .easeInOut(duration: animationDuration))
+                }
+                .activeButtonStyle
+                
+                Button(L10n.PlainOnboarding.Button.restoreWallet) {
+                    viewStore.send(.importExistingWallet, animation: .easeInOut(duration: animationDuration))
+                }
+                .frame(height: 80)
+                .foregroundColor(.black)
+            }
+            .padding(.all)
+            .navigationLinkEmpty(
+                isActive: viewStore.bindingForDestination(.importExistingWallet),
+                destination: {
+                    ImportWalletView(
+                        store: store.scope(
+                            state: \.importWalletState,
+                            action: OnboardingFlowReducer.Action.importWallet
+                        )
+                    )
+                }
+            )
+        }
+    }
+}
+
+struct PlainOnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            PlainOnboardingView(
+                store: Store(
+                    initialState: OnboardingFlowReducer.State(
+                        walletConfig: .default,
+                        importWalletState: .placeholder,
+                        nhImportWalletState: .placeholder,
+                        walletCreatedState: .placeholder
+                    ),
+                    reducer: OnboardingFlowReducer(saplingActivationHeight: 0)
+                )
+            )
+        }
+    }
+}